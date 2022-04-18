f581925ac75b3bf6659a6ee3480341583405f049
Irina <khrolenko.ira@gmail.com>
2022-04-18 17:07:38 +0300

    add variables to html file

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    print(dir(request))
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__isnull=False)
+    return render(request, 'blog/post_list.html', {"posts": posts})
